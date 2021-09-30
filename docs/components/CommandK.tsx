6a7800997388da839ebaccb22515ec7b8e63fb11
Jonas Arnklint <jonas.arnklint@gmail.com>
2021-09-30 13:39:52 +0200

    Add lint + fix docs error

--- docs/components/CommandK.tsx
+++ docs/components/CommandK.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 //import { applyPolyfills, defineCustomElements } from "../../packages/webcomponent/loader/index.cjs";
-import { defineCustomElements } from "../../packages/webcomponent";
+//import { defineCustomElements } from "../../packages/webcomponent";
 
 // this should be in the types in the dist of the component.. import them from there or add a package.json that refers to them
 declare global {
@@ -16,19 +16,20 @@ declare global {
 }
 
 const CommandK = ({
-  config
+  //config
 }) => {
   const c = useRef(null);
 
   // only run client side...
   useEffect(() => {
-    defineCustomElements();
+    //defineCustomElements();
     /*
     applyPolyfills().then(() => {
       console.log('polyfil')
     });
      */
 
+    /*
     const c = document.querySelector('my-component');
     if(c) {
       c.config = {
@@ -36,6 +37,7 @@ const CommandK = ({
         ...config
       };
     }
+     */
   }, []);
 
   //<my-component config={{ hey: "o" }}></my-component>

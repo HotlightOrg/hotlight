9558fe2554054952cb9eaab4dbd0b3725d5a6667
Jonas Arnklint <jonas.arnklint@gmail.com>
2021-10-16 09:06:08 +0200

    Update dev

--- packages/hotlight-react/dev/index.ts
+++ packages/hotlight-react/dev/index.ts
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Hotlight from '../src/index';
 
+const actions = [
+  { title: "Hotlight", trigger: "/" },
+  { title: "Readme", trigger: "/" },
+  { title: "Test", trigger: "/" },
+  { title: "My action", trigger: "/" },
+]
+
 ReactDOM.render(React.createElement(Hotlight,{
-  config: [],
-  actions: []
+  config: {},
+  actions
 }), document.querySelector('#root'));

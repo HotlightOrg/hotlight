eb893b3182f76fcb30fbc8d245654ff976bbffc5
Jonas Arnklint <jonas.arnklint@gmail.com>
2021-11-12 00:15:07 +0100

    move tests

--- packages/hotlight-react/__test__/Hotlight.spec.tsx
+++ packages/hotlight-react/__test__/Hotlight.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import Hotlight from "./Hotlight";
+import Hotlight from "../src/Hotlight";
 
 const config = {
   sources: [() => [{

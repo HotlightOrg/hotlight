eaf9a644bdb681d1a5b4b79625272d375693fbdd
Jonas Arnklint <jonas.arnklint@gmail.com>
2022-01-05 23:38:34 +0100

    Update react wrapper

--- packages/hotlight-react/src/hooks.ts
+++ packages/hotlight-react/src/hooks.ts
@@ -2,5 +2,10 @@ import { HotlightContext } from "./context";
 import { useContext } from "react";
 
 export const useHotlight = () => {
-  return useContext(HotlightContext);
+  try {
+    return useContext(HotlightContext);
+  } catch (e) {
+    console.warn("Make sure to add the <HotlightProvider> near the root of this application before using the useHotlight hook.");
+    console.warn(e);
+  }
 }
